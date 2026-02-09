99b2f4aa51b1ba459dc5d8270f7d9b5d34eaa2a7
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-02-09 23:13:13 +0530

    refactor: improve formatting and consistency in RoleSelector component

--- src/components/RoleSelector.tsx
+++ src/components/RoleSelector.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface RoleSelectorProps {
   label?: string;
   onRoleChange?: (role: string) => void;
 }
 
-const RoleSelector: React.FC<RoleSelectorProps> = ({ label = 'Who are you?', onRoleChange }) => {
-  const [selectedRole, setSelectedRole] = useState<string>('');
-  const roles = ['CEO', 'COO', 'CTO', 'CFO', 'CMO', 'LPD', 'GSD', 'AAD'];
+const RoleSelector: React.FC<RoleSelectorProps> = ({
+  label = "Who are you?",
+  onRoleChange,
+}) => {
+  const [selectedRole, setSelectedRole] = useState<string>("");
+  const roles = ["CEO", "COO", "CTO", "CFO", "CMO", "LPD", "GSD", "AAD"];
 
   const selectRole = (role: string) => {
     setSelectedRole(role);
@@ -16,17 +19,19 @@ const RoleSelector: React.FC<RoleSelectorProps> = ({ label = 'Who are you?', onR
 
   return (
     <div className="mb-6 max-w-2xl">
-      <label className="block text-2xl font-medium text-gray-900 mb-3">{label}</label>
-      <div className="flex lg:justify-between justify-start flex-wrap gap-2">
+      <label className="block text-2xl font-medium text-gray-900 mb-3">
+        {label}
+      </label>
+      <div className="flex lg:justify-between justify-start flex-wrap gap-1">
         {roles.map((role) => (
           <button
             key={role}
             type="button"
             onClick={() => selectRole(role)}
-            className={`h-18 w-18 rounded-lg font-normal text-lg transition-all duration-200 ${
+            className={`size-14 rounded-lg font-normal  transition-all duration-200 ${
               selectedRole === role
-                ? 'bg-(--dark-blue) text-white  hover:bg-gray-200'
-                : 'bg-[#EDEDED] text-(--dark-text-gray) hover:bg-[#EDEDED]'
+                ? "bg-(--dark-blue) text-white  hover:bg-gray-200"
+                : "bg-[#EDEDED] text-(--dark-text-gray) hover:bg-[#EDEDED]"
             }`}
           >
             {role}
